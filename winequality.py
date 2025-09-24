51d5cbafe22c65ae6172ed09ceb3b23f0fe91323
hor00 <ksbb85002@gmail.com>
2025-09-25 00:22:21 +0900

    Add winequality data processing script
    - Created a new script `winequality.py` for processing red and white wine quality datasets.
    - Implemented functions to read, modify, and save the datasets as CSV files.
    - Concatenated red and white wine data into a single DataFrame and saved it as `wine.csv`.
    - Added type labels for red and white wines in the respective DataFrames.

---
+++ winequality.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import time
+def making():
+    red_df.to_csv('winequality-red2.csv',index=False)
+    white_df.to_csv('winequality-white2.csv',index=False)
+def red():
+    red_df.head()
+    red_df.insert(0,column='type',value='red')
+    red_df.head()
+    red_df.shape
+def white():
+    white_df.head()
+    white_df.insert(0,column='type',value='white')
+    white_df.head()
+    white_df.shape
+def conect():
+    wine=pd.concat([red_df,white_df])
+    wine.shape
+    #win(wine)
+    #winsave(wine)
+    wine.to_csv('wine.csv',index=False)
+    #print(wine.info())
+    wine.columns=wine.columns.str.replace(' ','_')
+    wine.head()
+    wine.describe()
+    wine.to_csv('wine.csv',index=False)
+red_df=pd.read_csv('E:\mygit\winedate\winequality-red.csv',sep=';')
+white_df=pd.read_csv('E:\mygit\winedate\winequality-white.csv',sep=';')
+
+red()
+white()
+conect()
